5f85fff043490af192e7735455d5b532b14e671e
MihaylovRoman <ro-man700@mail.ru>
2023-11-27 19:55:09 +0500

    main page, list goods

---
+++ client/src/components/DeviceItem.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Card, Col, Image } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import { DEVICE_ROUTE } from '../utils/consts'
+const DeviceItem = ({ device }) => {
+    const navigate = useNavigate()
+    return (
+        <Col md={3} className='mt-4' onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)}>
+            <Card style={{ width: 150, cursor: 'pointer' }} border={'light'}>
+                <Image width={150} height={150} src={device.img} />
+                <div className='text-black-50 d-flex justify-content-between mt-2'>
+                    <div>
+                        Samsung
+                    </div>
+                    <div>
+                        <div className='d-flex'>
+                            {device.rating}
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    {device.name}
+                </div>
+            </Card>
+        </Col>
+    )
+}
+
+export default DeviceItem
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -1,41 +1,56 @@
-import {makeAutoObservable} from 'mobx'
+import { makeAutoObservable } from 'mobx'
 
 export default class DeviceStore {
-    constructor(){
+    constructor() {
         this._types = [
-            {id:1, name: 'Холодильники'},
-            {id:2, name: 'Смартфоны'}
+            { id: 1, name: 'Холодильники' },
+            { id: 2, name: 'Смартфоны' },
+            { id: 3, name: 'Ноутбуки' },
+            { id: 4, name: 'Телевизоры' },
         ]
         this._brands = [
-            {id:1, name: 'Samsung'},
-            {id:2, name: 'Apple'}
+            { id: 1, name: 'Samsung' },
+            { id: 2, name: 'Apple' }
         ]
         this._devices = [
-            {id:1, name: 'Iphone 12 PRO', price: 25000, rating: 5},
-            {id:2, name: 'Смартфоны'}
+            { id: 1, name: 'Iphone 12 PRO', price: 25000, rating: 5 },
+            { id: 2, name: 'Iphone 13' }
         ]
+        this._selectedType = {}
+        this._selectedBrand = {}
         makeAutoObservable(this)
     }
 
-    setTypes(types){
+    setTypes(types) {
         this._types = types
     }
-    setBrands(brands){
+    setBrands(brands) {
         this._brands = brands
     }
-    setDevices(devices){
+    setDevices(devices) {
         this._brands = devices
     }
+    setSelectedType(type) {
+        this._selectedType = type
+    }
+    setSelectedBrand(brand) {
+        this._selectedBrand = brand
+    }
 
-    
-    get types(){
+    get types() {
         return this._types
     }
 
-    get brands(){
+    get brands() {
         return this._brands
     }
-    get devices(){
+    get devices() {
         return this._devices
+    }
+    get selectedType() {
+        return this._selectedType
+    }
+    get selectedBrand() {
+        return this._selectedBrand
     }
 }
